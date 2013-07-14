5fea45dc0075c47764e226debc08b8d7a0ca4342
Imamura Jun <imachooon@gmail.com>
2013-07-14 16:28:47 +0000

    Modified web.js

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var app = express.createServer(express.logger());
 var buf = fs.readFileSync("input.html");
 var index = buf.toString();
 app.get('/', function(request, response) {
-  response.send("index");
+  response.send('index');
 });
 
 var port = process.env.PORT || 5000;
