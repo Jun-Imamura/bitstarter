a7db2c514bd99ffbf67a3c0e1f2cb9ed806da141
Imamura Jun <imachooon@gmail.com>
2013-07-15 10:29:47 +0000

    mod_1 grader.js

--- grader.js
+++ grader.js
@@ -25,17 +25,31 @@ var program = require('commander');
 var cheerio = require('cheerio');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
-var URL_DEFAULT = "http://quiet-scrubland-9023.herokuapp.com";
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
+    console.log('%s',instr);
     if(!fs.existsSync(instr)){
 	console.log("%s does not exist. Exiting.", instr);
 	process.exit(1); // http;//nodejs.org/api/process.html#process_process_exit_code
     }
-    return instr;
+    check_json_after_get(instr,'file');
+//    return instr;
 };
 
+var getUrl = function(apiurl){
+    rest.get(apiurl).on('complete',function(result){
+	if(result instanceof Error){
+	    console.log('Error: ' L result.message);
+	    process.exit(1);
+	} else{ 
+	    program.file = 'tmp.txt';
+	    fs.writeFileSync( program.file, result.toString());
+	    check_json_after_get(result, 'url');
+	}
+    })
+}
+		     
 var cheerioHtmlFile = function(htmlfile){
     return cheerio.load(fs.readFileSync(htmlfile));
 };
@@ -55,6 +69,11 @@ var checkHtmlFile = function(htmlfile, checksfile){
     return out;
 };
 
+var check_json_after_get = function(result,type){
+    var checkJson = checkHtmlFile(program.file, program.checks)
+    console.log(JSON.stringify(checkJson, null, 4));
+};
+
 
 var clone = function(fn){
     // Workaround for commaner.js issue.
@@ -65,12 +84,14 @@ var clone = function(fn){
 if(require.main == module){
     program
       .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
-      .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-      .option('-u, --url <url>', 'Path to url', clone(assertFileExists), URL_DEFAULT)
+      .option('-f, --file <html_file>', 'Path to index.html')
+      .option('-u, --url <html_file>', 'Link to url')
       .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if(program.url){
+	getUrl(program.url);
+    }else{
+	assertFileExists(program.file);
+    }
 }else{
     exports.checkHtmlFile = checkHtmlFile;
 }
